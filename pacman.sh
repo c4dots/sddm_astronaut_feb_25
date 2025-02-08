f415a4ee8cc490c9aeee001d191a24e24a6e2f02
c4vxl <141231805+c4vxl@users.noreply.github.com>
2025-02-08 12:11:22 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 DO_REBOOT=true
+DO_PREVIEW=true
 for ARG in "$@"; do
   case $ARG in
     --no-reboot)
       DO_REBOOT=false
+      ;;
+    *)
+  case $ARG in
+    --no-preview)
+      DO_PREVIEW=false
       ;;
     *)
       
@@ -19,6 +25,14 @@ sudo systemctl enable sddm.service
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
 
 sudo rm -R ~/sddm-astronaut-theme*
+
+
+if [[ "$DO_PREVIEW" == true ]]; then
+    read -p "Do you wish to see a preview? (y/n): " re
+    if [[ "$re" == "y" ]]; then
+        sddm-greeter-qt6 --test-mode --theme /usr/share/sddm/themes/sddm-astronaut-theme/
+    fi
+fi
 
 if [[ "$DO_REBOOT" == true ]]; then
     read -p "Do you wish to reboot now? (y/n): " re
