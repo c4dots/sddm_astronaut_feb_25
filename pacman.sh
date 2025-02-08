1d2fb8d5c3335750891bba7581153cf79cdd2509
c4vxl <141231805+c4vxl@users.noreply.github.com>
2025-02-08 11:52:55 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+cd ~
+
+DO_REBOOT=true
+for ARG in "$@"; do
+  case $ARG in
+    --no-reboot)
+      DO_REBOOT=false
+      ;;
+    *)
+      echo ">> Usage: $0 [--no-reboot]"
+      exit 1
+      ;;
+  esac
+done
+
+
 sudo pacman -S sddm
 sudo systemctl disable gdm.service
 sudo systemctl enable sddm.service
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
 
+sudo rm -R sddm-astronaut-theme*
+
 read -p "Do you wish to reboot now? (y/n): " re
-if [[ "$re" == "y" ]]; then
-    sudo reboot now
+if [[ "$DO_REBOOT" == true ]]; then
+    if [[ "$re" == "y" ]]; then
+        sudo reboot now
+    fi
 fi
